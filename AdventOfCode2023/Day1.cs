22604a411991dcbcbb26d3c4fbe7d582b3762ce5
Mark Heath <mark.heath@gmail.com>
2023-12-01 10:34:21 +0000

    day 1 part 2

--- AdventOfCode2023/Day1.cs
+++ AdventOfCode2023/Day1.cs
@@ -1,16 +1,37 @@
-﻿namespace AdventOfCode2023;
+﻿using SuperLinq;
+
+namespace AdventOfCode2023;
 
 // https://adventofcode.com/2023/day/1
 public class Day1 : ISolver
 {
-    public (string, string) ExpectedResult => ("53386", "");
+    public (string, string) ExpectedResult => ("53386", "53312");
+
+    private static readonly string[] digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
     public (string, string) Solve(string[] input)
     {
         var part1 = input
                         .Select(line => $"{line.First(c => char.IsDigit(c))}{line.Last(c => char.IsDigit(c))}")
-                        .Select(int.Parse)
-                        .Sum();
-        return (part1.ToString(), "");
+                        .Sum(int.Parse);
+        var part2 = input
+                        .Select(line => $"{FindDigit(line, false)}{FindDigit(line, true)}")
+                        .Sum(int.Parse);
+        
+        return (part1.ToString(), part2.ToString());
+    }
+
+    private int FindDigit(string input, bool reverse)
+    {
+        var indexes = Enumerable.Range(0, input.Length);
+        if (reverse) indexes = indexes.Reverse();
+        foreach (var n in indexes)
+        {            
+            if (char.IsDigit(input[n])) return input[n] - '0';
+            var matchedDigit = digits.FirstOrDefault(d => input.Substring(n).StartsWith(d));
+            if (matchedDigit != null)
+                return digits.IndexOf(matchedDigit) + 1;
+        }
+        throw new ArgumentException($"No digits found in {input}");
     }
 }
--- UnitTests/Day1Tests.cs
+++ UnitTests/Day1Tests.cs
@@ -14,6 +14,21 @@ a1b2c3d4e5f
 treb7uchet".Split("\r\n");
         var solver = new Day1();
         var solution = solver.Solve(testInput);
-        Assert.That(solution, Is.EqualTo(("142", "")));
+        Assert.That(solution, Is.EqualTo(("142", "142")));
+    }
+    [Test]
+    public void SolvePart2WithTestInput()
+    {
+        var testInput = @"two1nine
+eightwo1three
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen".Split("\r\n");
+        var solver = new Day1();
+        var (_,p2) = solver.Solve(testInput);
+        Assert.That(p2, Is.EqualTo("281"));
     }
 }
+
