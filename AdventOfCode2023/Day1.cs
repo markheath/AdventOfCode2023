65493388fd8823ef306bcfac198e0c3ac681b2a5
Mark Heath <mark.heath@gmail.com>
2023-12-01 10:18:48 +0000

    day 1 part 1

--- AdventOfCode2023/Day1.cs
+++ AdventOfCode2023/Day1.cs
@@ -3,10 +3,14 @@
 // https://adventofcode.com/2023/day/1
 public class Day1 : ISolver
 {
-    public (string, string) ExpectedResult => ("", "");
+    public (string, string) ExpectedResult => ("53386", "");
 
     public (string, string) Solve(string[] input)
     {
-        throw new NotImplementedException();
+        var part1 = input
+                        .Select(line => $"{line.First(c => char.IsDigit(c))}{line.Last(c => char.IsDigit(c))}")
+                        .Select(int.Parse)
+                        .Sum();
+        return (part1.ToString(), "");
     }
 }
--- UnitTests/Day1Tests.cs
+++ UnitTests/Day1Tests.cs
@@ -8,9 +8,12 @@ public class Day1Tests
     [Test]
     public void SolveWithTestInput()
     {
-        var testInput = @"".Split("\r\n");
+        var testInput = @"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet".Split("\r\n");
         var solver = new Day1();
         var solution = solver.Solve(testInput);
-        Assert.That(solution, Is.EqualTo(("", "")));
+        Assert.That(solution, Is.EqualTo(("142", "")));
     }
 }
