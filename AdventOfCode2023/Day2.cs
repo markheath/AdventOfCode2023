fc924044886c09505b7cb78b284e09b51d3f33cf
Mark Heath <mark.heath@gmail.com>
2023-12-02 13:12:05 +0000

    day 2 part 2

--- AdventOfCode2023/Day2.cs
+++ AdventOfCode2023/Day2.cs
@@ -4,11 +4,12 @@ namespace AdventOfCode2023;
 
 public class Day2 : ISolver
 {
-    public (string, string) ExpectedResult => ("2528", "");
+    public (string, string) ExpectedResult => ("2528", "67363");
 
     public (string, string) Solve(string[] input)
     {
         var part1 = 0;
+        var part2 = 0;
         foreach(var game in input)
         {
             var gameNumber = int.Parse(Regex.Match(game, @"Game (\d+)").Groups[1].Value);
@@ -16,8 +17,13 @@ public class Day2 : ISolver
                  .ToDictionaryWithCombiner(m => m.Groups[2].Value,
                     m => int.Parse(m.Groups[1].Value), Math.Max);
             var possible = max["red"] <= 12 && max["green"] <= 13 && max["blue"] <= 14;
-            if (possible) part1 += gameNumber;           
+            if (possible)
+            {
+                part1 += gameNumber;
+            }
+            part2 += max["red"] * max["green"] * max["blue"];
+
         }
-        return (part1.ToString(), "");
+        return (part1.ToString(), part2.ToString());
     }
 }
--- UnitTests/Day2Tests.cs
+++ UnitTests/Day2Tests.cs
@@ -16,7 +16,7 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green".Split(Environment.NewLine);
         var solver = new Day2();
         var solution = solver.Solve(testInput);
-        Assert.That(solution, Is.EqualTo(("8", "")));
+        Assert.That(solution, Is.EqualTo(("8", "2286")));
     }
 }
 
